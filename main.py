11ece6bb7ad08922624ad16cdef985577c966d20
Arek <arek.mroczkow@gmail.com>
2023-11-02 01:43:31 +0100

    Added colors to text and some QOL changes

--- main.py
+++ main.py
@@ -12,10 +12,20 @@ GAME_OVER = False
 
 game_speeds = {"slow": 0.15, "normal": 0.1, "fast": 0.05}
 difficulty_levels = {"easy": 5, "normal": 10, "hard": 20}
+score_multipliers = {"easy": 1, "normal": 2, "hard": 5}
+
+
+def increase_score(current_score):
+    difficulty = list(difficulty_levels.keys())[list(difficulty_levels.values()).index(RAND_ENEMY)]
+    return current_score + score_multipliers[difficulty]
 
 
 def show_settings(stdscr, settings):
     global ENEMY_SPEED, RAND_ENEMY
+
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
     options = [
         ("Game Speed", list(game_speeds.keys())[list(game_speeds.values()).index(ENEMY_SPEED)]),
@@ -38,13 +48,13 @@ def show_settings(stdscr, settings):
         stdscr.clear()
 
         #Border
-        for y in range(box_height):
-            if y == 0 or y == box_height - 1:
+        for y in range(box_height + 3):
+            if y == 0 or y == box_height + 2:
                 stdscr.addstr(start_y + y, start_x, "+" + "-" * (box_width - 2) + "+")
             else:
                 stdscr.addstr(start_y + y, start_x, "|" + " " * (box_width - 2) + "|")
 
-        stdscr.addstr(start_y + 1, start_x + 4, "Settings: ", curses.A_BOLD)
+        stdscr.addstr(start_y + 1, start_x + 4, "Settings: ", curses.color_pair(1))
         stdscr.addstr(start_y + 3, start_x + 4, "Use arrows '<-''->' to manipulate settings")
 
         for index, (text, value) in enumerate(options):
@@ -53,7 +63,7 @@ def show_settings(stdscr, settings):
             else:
                 stdscr.addstr(start_y + 5 + index * 2, start_x + 4, f"  {text}: {value}")
 
-        stdscr.addstr(start_y + 5 + len(options) * 2, start_x + 4, "Press Enter to return")
+        stdscr.addstr(start_y + 6 + len(options) * 2, start_x + 4, "Press Enter to return to the main menu", curses.color_pair(2))
         key = stdscr.getch()
 
 
@@ -91,6 +101,10 @@ def show_settings(stdscr, settings):
 
 
 def show_information(stdscr):
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+
     while True:
         stdscr.clear()
 
@@ -99,8 +113,12 @@ def show_information(stdscr):
 
         information_text = "Game is all about dodging incoming bullets, the longer you survive the more points you get." \
                            "\n You have only one life." \
-                           " \n\n\n How to play: \n\n Press '<-' to move left \n Press '->' to move right " \
-                           "\n\n\n Press enter to return to the main menu"
+                           " \n\n\n How to play: \n\n Press '<-' to move left \n Press '->' to move right \n\n" \
+                           "Score multipliers depending on the game difficulty:  \n\n"\
+                            "Easy - Points x1\n"\
+                            "Normal - Points x2\n"\
+                            "Hard - Points x5\n"\
+                            "\n\n Press enter to return to the main menu"
 
 
         #Info box
@@ -110,18 +128,32 @@ def show_information(stdscr):
         start_y = (sh - info_height) // 2
         start_x = (sw - info_width) // 2
 
+
         border_width = info_width + 4
         border_height = info_height
 
-        border = '+' + '-' * (border_width - 2) + '+'
         stdscr.addstr(start_y - 2, start_x - 2, '+' + '-' * (border_width) + '+')
         stdscr.addstr(start_y + border_height + 1, start_x - 2, '+' + '-' * (border_width) + '+')
+
         for i in range(start_y - 1, start_y + border_height + 1):
             stdscr.addch(i, start_x - 2, '|')
-            stdscr.addch(i, start_x + border_width - 2, '|')
+            stdscr.addch(i, start_x + border_width - 1, '|')
 
         for i, line in enumerate(information_text.split('\n')):
             stdscr.addstr(start_y + i, start_x, line.center(info_width + 2))
+
+        #Add color to items from list
+        yellow_lines = ["How to play:", "Score multipliers depending on the game difficulty:"]
+        for i, line in enumerate(information_text.split('\n')):
+            if line.strip() in yellow_lines:
+                stdscr.addstr(start_y + i, start_x, line.center(info_width + 2), curses.color_pair(1))
+            else:
+                stdscr.addstr(start_y + i, start_x, line.center(info_width + 2))
+
+        green_lines = ["Press enter to return to the main menu"]
+        for i, line in enumerate(information_text.split('\n')):
+            if line.strip() in green_lines:
+                stdscr.addstr(start_y + i, start_x, line.center(info_width + 2), curses.color_pair(2))
 
         stdscr.refresh()
         key = stdscr.getch()
@@ -136,6 +168,8 @@ def show_welcome_screen(stdscr):
     stdscr.refresh()
     sh, sw = stdscr.getmaxyx()
 
+
+
     logo = [
         "  _    _  _  ______  _       ______   _____   _____  ",
         " | |  | || ||__  __|| |     |  ____| / ____| / ____| ",
@@ -146,7 +180,7 @@ def show_welcome_screen(stdscr):
     ]
 
     #Menu
-    menu_items = ["1. Start the game", "2. Information", "3. Settings", "4. Leaderboard", "5. Quit"]
+    menu_items = ["Start the game", "Information", "Settings", "Leaderboard", "Quit"]
     menu_height = len(menu_items)
     menu_width = max(len(item) for item in menu_items)
 
@@ -166,8 +200,7 @@ def show_welcome_screen(stdscr):
         for i, line in enumerate(logo):
             stdscr.addstr(start_y + i, start_x, line)
 
-        #Menu box
-        border_width = max(logo_width, menu_width) + 8
+        border_width = max(logo_width, menu_width) + 4
         border_height = logo_height + menu_height + 3
 
         border = '+' + '-' * (border_width - 2) + '+'
@@ -177,11 +210,15 @@ def show_welcome_screen(stdscr):
             stdscr.addch(i, start_x - 3, '|')
             stdscr.addch(i, start_x + border_width - 2, '|')
 
+        # Calculate centering for menu items
+        menu_start_x = start_x + (border_width // 2)
         for i, item in enumerate(menu_items):
+            item_width = len(item)
+            menu_start_x = start_x + (border_width - item_width - 3) // 2
             if i == selected_option:
-                stdscr.addstr(start_y + logo_height + i + padding, start_x, '|  ' + item + '  |', curses.A_STANDOUT)
+                stdscr.addstr(start_y + logo_height + i + padding, menu_start_x, item, curses.A_STANDOUT)
             else:
-                stdscr.addstr(start_y + logo_height + i + padding, start_x, '|  ' + item + '  |')
+                stdscr.addstr(start_y + logo_height + i + padding, menu_start_x, item)
 
         stdscr.refresh()
 
@@ -196,6 +233,10 @@ def show_welcome_screen(stdscr):
             return selected_option
 
 def ask_for_name(stdscr, score):
+    curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+
+
     stdscr.clear()
 
     game_over_text = [
@@ -221,9 +262,20 @@ def ask_for_name(stdscr, score):
     for idx, line in enumerate(game_over_text, start=1):
         stdscr.addstr(y + idx, x + 2, line)
 
-    stdscr.addstr(19, 35, "Enter your name: ")
+        # Display the score
+        score_text = f"Score: {score}"
+        score_x = x + (w - len(score_text)) // 2
+        stdscr.addstr(y + h + 1, score_x, score_text, curses.color_pair(1))
+
+
     curses.echo()
-    name = stdscr.getstr(19, 53, 20).decode("utf-8")
+    name = ""
+    #Asks for name untill its not empty
+    while not name.strip():
+        stdscr.addstr(19, 35, "Enter your name: ", curses.color_pair(2))
+        name = stdscr.getstr(19, 53, 20).decode("utf-8")
+        if not name.strip():
+            stdscr.addstr(23, 35, "Name cannot be empty! Please enter again.")
     curses.noecho()
     return name
 
@@ -242,6 +294,10 @@ def game_over_screen(stdscr, score):
 
 
 def show_leaderboard(stdscr, settings):
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+
     while True:
         stdscr.clear()
         h, w = stdscr.getmaxyx()
@@ -254,7 +310,7 @@ def show_leaderboard(stdscr, settings):
             line_content_length = len(f"{name} - {score} (Speed: {game_speed}, Difficulty: {difficulty_level})")
             max_width = max(line_content_length + 20, max_width)  # Adjust as needed
 
-        border_top_bottom = "+" + "=" * (max_width - 2) + "+"
+        border_top_bottom = "+" + "-" * (max_width - 2) + "+"
         start_x = (w - max_width) // 2
         start_y = (h - len(LEADERBOARD) - 5) // 2
 
@@ -262,10 +318,10 @@ def show_leaderboard(stdscr, settings):
         stdscr.addstr(start_y, start_x, border_top_bottom)
 
         stdscr.addstr(start_y + 1, start_x, "|")
-        stdscr.addstr(start_y + 1, start_x + 1, "Leaderboard".center(max_width - 2), curses.A_BOLD)
+        stdscr.addstr(start_y + 1, start_x + 1, "Leaderboard".center(max_width - 2), curses.color_pair(1))
         stdscr.addstr(start_y + 1, start_x + max_width - 1, "|")
         stdscr.addstr(start_y + 2, start_x, "|")
-        stdscr.addstr(start_y + 2, start_x + 1, "===========".center(max_width - 2), curses.A_BOLD)
+        stdscr.addstr(start_y + 2, start_x + 1, "===========".center(max_width - 2), curses.color_pair(1))
         stdscr.addstr(start_y + 2, start_x + max_width - 1, "|")
 
         #Entries with borders
@@ -279,7 +335,7 @@ def show_leaderboard(stdscr, settings):
         #Instructions inside the border
         instruction_index = start_y + 4 + len(LEADERBOARD)
         stdscr.addstr(instruction_index, start_x, "|")
-        stdscr.addstr(instruction_index, start_x + 1, "Press enter key to return to the menu.".ljust(max_width - 2))
+        stdscr.addstr(instruction_index, start_x + 1, "Press enter key to return to the menu".ljust(max_width - 2), curses.color_pair(2))
         stdscr.addstr(instruction_index, start_x + max_width - 1, "|")
 
         #side borders for leaderboard
@@ -300,15 +356,19 @@ def show_leaderboard(stdscr, settings):
 
 
 def main_game(stdscr):
-    global GAME_OVER
+    global GAME_OVER, game_speeds, difficulty_levels
     GAME_OVER = False
     curses.curs_set(0)
     stdscr.clear()
     stdscr.timeout(0)
     stdscr.refresh()
 
-    # Arena dimensions
-    sh, sw = 20, 40
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+
+    #Arena dimensions
+    sh, sw = 30, 40
 
     # Calculate screen center
     screen_h, screen_w = stdscr.getmaxyx()
@@ -326,14 +386,14 @@ def main_game(stdscr):
 
     for _ in range(num_enemies):
         enemy = 'v'
-        enemy_x = random.randint(1, sw - 2)
+        enemy_x = random.randint(1, sw - 3)
         enemy_y = 2
         enemies.append((enemy_x, enemy_y, enemy))
 
-    #Arena ascii border
-    top_bottom_border = '+' + '-' * (sw - 4) + '+'
-    side_border = '|' + ' ' * (sw - 4) + '|'
-    ascii_art = [' ' * (sw - 2), top_bottom_border] + [side_border] * (sh - 4) + [top_bottom_border]
+        # Arena ascii border
+        top_bottom_border = '+' + '-' * (sw - 4) + '+'
+        side_border = '|' + ' ' * (sw - 4) + '|'
+        ascii_art = [' ' * (sw - 2), top_bottom_border] + [side_border] * (sh - 4) + [top_bottom_border]
 
     score = 0
 
@@ -368,7 +428,7 @@ def main_game(stdscr):
         if random.randint(1, RAND_ENEMY) > 4:
             new_enemy = 'v'
             new_enemy_x = random.randint(2, sw - 3)
-            new_enemy_y = 0
+            new_enemy_y = 2
             new_enemies.append((new_enemy_x, new_enemy_y, new_enemy))
 
         enemies = new_enemies
@@ -378,13 +438,16 @@ def main_game(stdscr):
             GAME_OVER = True
 
         # Draw player
+        win.attron(curses.color_pair(2))
         win.addstr(player_y, player_x, player)
+        win.attroff(curses.color_pair(2))
 
-        # Draw enemies
         for enemy_x, enemy_y, enemy in enemies:
+            win.attron(curses.color_pair(1))
             win.addch(enemy_y, enemy_x, enemy)
+            win.attroff(curses.color_pair(1))
             if enemy_y == player_y:
-                score += 1
+                score = increase_score(score)
 
         win.addstr(0, 2, f'Score: {score}')
 
@@ -421,19 +484,16 @@ def main(stdscr, settings=game_speeds):
             stdscr.addstr(0, 0, "Settings:")
             show_settings(stdscr, settings)
             stdscr.refresh()
-            stdscr.getch()
 
         elif choice == 3:
             stdscr.clear()
             stdscr.addstr(0, 0, "Leaderboard")
             show_leaderboard(stdscr, settings)
             stdscr.refresh()
-            stdscr.getch()
 
         elif choice == 4:
             stdscr.addstr(0, 0, "Goodbye!")
             stdscr.refresh()
-            stdscr.getch()
             break
 
 
