d5ad307f0b0afc64bc31571a2b766517c47ff0ba
Arek <arek.mroczkow@gmail.com>
2023-11-06 01:29:17 +0100

    Settings to change border colors added

--- main.py
+++ main.py
@@ -10,19 +10,48 @@ RAND_ENEMY = 5
 LEADERBOARD = []
 GAME_OVER = False
 
+COLOR_CONSTANTS = {
+    "yellow": curses.COLOR_YELLOW,
+    "green": curses.COLOR_GREEN,
+    "red": curses.COLOR_RED,
+    "blue": curses.COLOR_BLUE,
+    "cyan": curses.COLOR_CYAN,
+    "magenta": curses.COLOR_MAGENTA,
+    "white": curses.COLOR_WHITE,
+}
+
+COLOR_PAIRS = {
+    "yellow": 1,
+    "green": 2,
+    "red": 3,
+    "blue": 4,
+    "cyan": 5,
+    "magenta": 6,
+    "white": 7,
+}
+
+CURRENT_COLOR = "yellow"
 
 game_speeds = {"slow": 0.15, "normal": 0.1, "fast": 0.05}
 difficulty_levels = {"easy": 5, "normal": 10, "hard": 20}
 score_multipliers = {"easy": 1, "normal": 2, "hard": 5}
+
+def init_colors():
+    if curses.can_change_color():
+        for name, color in COLOR_CONSTANTS.items():
+            curses.init_pair(COLOR_PAIRS[name], color, curses.COLOR_BLACK)
+    else:
+        pass
 
 
 def increase_score(current_score):
     difficulty = list(difficulty_levels.keys())[list(difficulty_levels.values()).index(RAND_ENEMY)]
     return current_score + score_multipliers[difficulty]
 
-def draw_border(stdscr, start_x, start_y, width, height, border_type=0):
-    # Define color pairs if needed, for different border styles
-    # curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)  # Example color pair
+def draw_border(stdscr, start_x, start_y, width, height):
+    global COLOR_PAIRS, COLOR_CONSTANTS, CURRENT_COLOR
+    color_pair = COLOR_PAIRS[CURRENT_COLOR]
+    stdscr.attron(curses.color_pair(color_pair))
 
     # Draw the top and bottom borders
     stdscr.addstr(start_y, start_x, "+" + "-" * (width) + "+")
@@ -33,86 +62,94 @@ def draw_border(stdscr, start_x, start_y, width, height, border_type=0):
         stdscr.addstr(y, start_x, "|")
         stdscr.addstr(y, start_x + width + 1, "|")
 
+    stdscr.attroff(curses.color_pair(color_pair))
+
 
 def show_settings(stdscr, settings):
-    global ENEMY_SPEED, RAND_ENEMY
-
-    curses.start_color()
+    global ENEMY_SPEED, RAND_ENEMY, CURRENT_COLOR, COLOR_PAIRS
     curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
     options = [
         ("Game Speed", list(game_speeds.keys())[list(game_speeds.values()).index(ENEMY_SPEED)]),
-        ("Difficulty", list(difficulty_levels.keys())[list(difficulty_levels.values()).index(RAND_ENEMY)])
+        ("Difficulty", list(difficulty_levels.keys())[list(difficulty_levels.values()).index(RAND_ENEMY)]),
+        ("Border Color", CURRENT_COLOR)
     ]
 
-    #Settings box
+    # Calculate settings box dimensions
     max_width = max([len(f"{text}: {value}") for text, value in options])
-    max_width = max(max_width, len("Use '-' or '+' to manipulate settings"))
+    max_width = max(max_width, len("Use arrows '<-' and '->' to manipulate settings"))
     box_width = max_width + 10
     box_height = len(options) * 2 + 5
 
+    # Get window's height and width
     height, width = stdscr.getmaxyx()
 
+    # Calculate the starting position of the settings box
     start_y = (height - box_height) // 2
     start_x = (width - box_width) // 2
 
     selected_option = 0
+
     while True:
         stdscr.clear()
 
-        #Border
-        draw_border(stdscr, start_x, start_y, box_width, box_height + 3)
+        # Draw the border with the selected color
+        draw_border(stdscr, start_x, start_y, box_width, box_height + 1)
 
+        stdscr.addstr(start_y + 1, start_x + 2, "Settings: ", curses.color_pair(1))
+        stdscr.addstr(start_y + 3, start_x + 2, "Use arrows '<-' and '->' to manipulate settings")
 
-        stdscr.addstr(start_y + 1, start_x + 4, "Settings: ", curses.color_pair(1))
-        stdscr.addstr(start_y + 3, start_x + 4, "Use arrows '<-''->' to manipulate settings")
-
+        # Display the options
         for index, (text, value) in enumerate(options):
             if index == selected_option:
-                stdscr.addstr(start_y + 5 + index * 2, start_x + 4, f"> {text}: {value}", curses.A_STANDOUT)
+                stdscr.addstr(start_y + 5 + index * 2, start_x + 2, f"> {text}: {value}", curses.A_STANDOUT)
             else:
-                stdscr.addstr(start_y + 5 + index * 2, start_x + 4, f"  {text}: {value}")
+                stdscr.addstr(start_y + 5 + index * 2, start_x + 2, f"  {text}: {value}")
 
-        stdscr.addstr(start_y + 6 + len(options) * 2, start_x + 4, "Press Enter to return to the main menu", curses.color_pair(2))
+        stdscr.addstr(start_y + 6 + len(options) * 2, start_x + 2, "Press Enter to return to the main menu", curses.color_pair(2))
         key = stdscr.getch()
 
-
-        # Settings option selection
+        # Handle user input for option selection
         if key == curses.KEY_UP and selected_option > 0:
             selected_option -= 1
         elif key == curses.KEY_DOWN and selected_option < len(options) - 1:
             selected_option += 1
-        elif key == curses.KEY_RIGHT:
+        elif key == curses.KEY_RIGHT or key == curses.KEY_LEFT:
             if selected_option == 0:
-                current_speed = options[selected_option][1]
-                next_speed_index = (list(game_speeds.keys()).index(current_speed) + 1) % len(game_speeds)
+                # Change game speed
+                current_speed_index = list(game_speeds.keys()).index(options[selected_option][1])
+                next_speed_index = (current_speed_index + 1) % len(game_speeds) if key == curses.KEY_RIGHT else (current_speed_index - 1) % len(game_speeds)
                 ENEMY_SPEED = game_speeds[list(game_speeds.keys())[next_speed_index]]
-            else:
-                current_difficulty = options[selected_option][1]
-                next_difficulty_index = (list(difficulty_levels.keys()).index(current_difficulty) + 1) % len(
-                    difficulty_levels)
+                options[selected_option] = ("Game Speed", list(game_speeds.keys())[next_speed_index])
+            elif selected_option == 1:
+                # Change difficulty
+                current_difficulty_index = list(difficulty_levels.keys()).index(options[selected_option][1])
+                next_difficulty_index = (current_difficulty_index + 1) % len(difficulty_levels) if key == curses.KEY_RIGHT else (current_difficulty_index - 1) % len(difficulty_levels)
                 RAND_ENEMY = difficulty_levels[list(difficulty_levels.keys())[next_difficulty_index]]
-        elif key == curses.KEY_LEFT:
-            if selected_option == 0:
-                current_speed = options[selected_option][1]
-                prev_speed_index = (list(game_speeds.keys()).index(current_speed) - 1) % len(game_speeds)
-                ENEMY_SPEED = game_speeds[list(game_speeds.keys())[prev_speed_index]]
-            else:
-                current_difficulty = options[selected_option][1]
-                prev_difficulty_index = (list(difficulty_levels.keys()).index(current_difficulty) - 1) % len(
-                    difficulty_levels)
-                RAND_ENEMY = difficulty_levels[list(difficulty_levels.keys())[prev_difficulty_index]]
-        elif key == 10:
+                options[selected_option] = ("Difficulty", list(difficulty_levels.keys())[next_difficulty_index])
+            elif selected_option == 2:
+                # Change border color
+                color_names = list(COLOR_CONSTANTS.keys())
+                current_color_index = color_names.index(CURRENT_COLOR)
+                new_color_index = (current_color_index + (1 if key == curses.KEY_RIGHT else -1)) % len(color_names)
+                CURRENT_COLOR = color_names[new_color_index]
+                options[selected_option] = ("Border Color", CURRENT_COLOR)
+                curses.init_pair(COLOR_PAIRS[CURRENT_COLOR], COLOR_CONSTANTS[CURRENT_COLOR], curses.COLOR_BLACK)
+
+
+
+        if key == 10:
             return
 
         options[0] = ("Game Speed", list(game_speeds.keys())[list(game_speeds.values()).index(ENEMY_SPEED)])
         options[1] = ("Difficulty", list(difficulty_levels.keys())[list(difficulty_levels.values()).index(RAND_ENEMY)])
+        options[2] = ("Border Color", CURRENT_COLOR)
 
+        stdscr.refresh()
 
 
 def show_information(stdscr):
-    curses.start_color()
     curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
@@ -197,11 +234,29 @@ def show_welcome_screen(stdscr):
     start_y = (sh - (logo_height + menu_height + padding)) // 2
     start_x = (sw - max(logo_width, menu_width)) // 2
 
+    hint_msg = "Use Arrow Up and Down to move"
+    hint_width = sw // 2  # Make the subwin half the width of the screen
+    hint_height = 4
+
+    hint_start_y = sh - hint_height  # Position the subwin just above the bottom
+    hint_start_x = (sw - hint_width) // 2  # Center the subwin horizontally
+    hint_subwin = stdscr.subwin(hint_height, hint_width, hint_start_y, hint_start_x)
+
+    hint_subwin.addstr(1, (hint_width - len(hint_msg)) // 2, hint_msg)
+    hint_subwin.box()
+
     selected_option = 0
 
     while True:
         stdscr.clear()
 
+        hint_subwin.clear()  # Clear the subwin
+        hint_subwin.box()
+        hint_subwin.addstr(0, 2, "Hint")
+        hint_subwin.addstr(1, 2, "Use Arrow Up '^' and Down 'v' to move in the menu")
+        hint_subwin.addstr(2, 2, "Press 'Enter' to choose an option")
+
+        hint_subwin.refresh()
         #Show logo
         for i, line in enumerate(logo):
             stdscr.addstr(start_y + i, start_x, line)
@@ -270,10 +325,10 @@ def ask_for_name(stdscr, score):
     name = ""
     #Asks for name untill its not empty
     while not name.strip():
-        stdscr.addstr(19, 35, "Enter your name: ", curses.color_pair(2))
-        name = stdscr.getstr(19, 53, 20).decode("utf-8")
+        stdscr.addstr(21, 35, "Enter your name: ", curses.color_pair(2))
+        name = stdscr.getstr(21, 53, 20).decode("utf-8")
         if not name.strip():
-            stdscr.addstr(23, 35, "Name cannot be empty! Please enter again.")
+            stdscr.addstr(25, 35, "Name cannot be empty! Please enter again.")
     curses.noecho()
     return name
 
@@ -292,7 +347,6 @@ def game_over_screen(stdscr, score):
 
 
 def show_leaderboard(stdscr, settings):
-    curses.start_color()
     curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
@@ -306,7 +360,7 @@ def show_leaderboard(stdscr, settings):
         #Content border calculation
         for _, (name, score, game_speed, difficulty_level) in enumerate(LEADERBOARD):
             line_content_length = len(f"{name} - {score} (Speed: {game_speed}, Difficulty: {difficulty_level})")
-            max_width = max(line_content_length + 20, max_width)  # Adjust as needed
+            max_width = max(line_content_length + 20, max_width)
 
         start_x = (w - max_width) // 2
         start_y = (h - len(LEADERBOARD) - 5) // 2
@@ -345,7 +399,6 @@ def main_game(stdscr):
     stdscr.timeout(0)
     stdscr.refresh()
 
-    curses.start_color()
     curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
 
@@ -441,13 +494,17 @@ def main_game(stdscr):
 
 
 def main(stdscr, settings=game_speeds):
+    curses.start_color()
+    init_colors()
     curses.curs_set(0)
     sh, sw = 20, 40
     stdscr.clear()
     stdscr.refresh()
 
+
     while True:
         choice = show_welcome_screen(stdscr)
+
 
         if choice == 0:
             stdscr.clear()
@@ -456,16 +513,20 @@ def main(stdscr, settings=game_speeds):
 
         elif choice == 1:
             stdscr.clear()
+            init_colors()
             show_information(stdscr)
             stdscr.refresh()
 
         elif choice == 2:
             stdscr.clear()
+            init_colors()
             show_settings(stdscr, settings)
             stdscr.refresh()
 
+
         elif choice == 3:
             stdscr.clear()
+            init_colors()
             show_leaderboard(stdscr, settings)
             stdscr.refresh()
 
